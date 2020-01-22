b2a1ab37a2ef404947777e0eb1451d452c28735b
Jacek Kurbiel <jacek@softnauts.com>
2020-01-22 13:36:27 +0100

    add pods versio 0.0.1

---
+++ testSDKpod/testSDKpod/TestGG.swift
@@ -0,0 +1,17 @@
+//
+//  TestGG.swift
+//  testPods
+//
+//  Created by Jacek Kurbiel on 22/01/2020.
+//  Copyright © 2020 Jacek Kurbiel. All rights reserved.
+//
+
+import Foundation
+
+public class TestGG {
+    
+    public static func addValueGG(value1: Int, value2: Int) -> Int {
+        print("value1 + value2 = \(value1 + value2)")
+        return value1 + value2
+    }
+}
---
+++ testSDKpod/testSDKpod/testSDKpod.h
@@ -0,0 +1,19 @@
+//
+//  testSDKpod.h
+//  testSDKpod
+//
+//  Created by Jacek Kurbiel on 22/01/2020.
+//  Copyright © 2020 Jacek Kurbiel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testSDKpod.
+FOUNDATION_EXPORT double testSDKpodVersionNumber;
+
+//! Project version string for testSDKpod.
+FOUNDATION_EXPORT const unsigned char testSDKpodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testSDKpod/PublicHeader.h>
+
+
